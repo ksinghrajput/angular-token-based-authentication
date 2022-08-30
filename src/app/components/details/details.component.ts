f962b13e365e82fbd97c3bfbff6c8bbb9d1ce624
kishan_ideacrest <kishan.singh@ideacrestsolutions.com>
2022-08-30 17:13:25 +0530

    Updated code to get correct response and refactored

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service'
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  userData: any;
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.auth.getUserDetails().subscribe(res => {
+      this.userData = JSON.parse(JSON.stringify(res))
+      console.log("Kishan", res);
+    })
   }
 
 }
